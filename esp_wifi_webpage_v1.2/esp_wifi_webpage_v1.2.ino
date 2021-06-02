87650ae8c6d072a7ecb9bdc47b008cb458c4e323
Tux <ruster1506@gmail.com>
2021-06-02 10:59:05 -0400

    Comunicação arduino -> esp (testar)

--- esp_wifi_webpage_v1.2/esp_wifi_webpage_v1.2.ino
+++ esp_wifi_webpage_v1.2/esp_wifi_webpage_v1.2.ino
@@ -12,12 +12,33 @@ ESP8266WiFiMulti wifiMulti;     // Obejto para gerenciar credenciais de rede
 ESP8266WebServer server(80);    // Webserver para utilização do protocolo HTTP na porta 80
 WebSocketsServer webSocket(81); // WebSocket na porta 81
 
+//====================================================================
+
+//variaveis para comunicação no sentindo arduino -> ESP
+
+const byte numChars = 32;       //Número máxim de caracteres por mensagem
+char receivedChars[numChars];   //Armazenar mensagem
+char tempChars[numChars];       // Array temporário para parsing
+
+
+// Armazenar mensagem depois do parsing
+
+char messageFromPC[numChars] = {0};
+int integerFromPC = 0;
+int integerFromPC2 = 0;
+
+//Adicionar mais conforme necessidade
+//float floatFromPC = 0.0;
+
+boolean newData = false;
+
+//====================================================================
+
 // Protótipo das funções
 
 void startWiFi();
 void startMDNS();
 void startServer();
-void webSocketEvent();
 void sendMessageWs();
 
 // id da conexão atual -> é atualizado no callback do websocket (on event)
@@ -56,7 +77,14 @@ void loop() {
   MDNS.update();
   server.handleClient();
   webSocket.loop();
-
+ 
+  recvWithStartEndMarkers();
+  if (newData == true) {
+    strcpy(tempChars, receivedChars);
+    parseData();
+    sendMessageWs(id, messageFromPC);
+    newData = false; //Esperar por nova mensagem
+  }
 }
 
 void startWiFi(){
@@ -163,4 +191,59 @@ bool handleFileRead(String path){  // Serve o arquivo correto para o client (se 
   }
   Serial.println(String("\tFile Not Found: ") + path);
   return false;                                          // Se o arquivo não existir retornar false
+
+}
+
+//=========================================================================================================
+
+void recvWithStartEndMarkers(){
+    static boolean recvInProgress = false;
+    static byte ndx = 0;
+    char startMarker = '<';
+    char endMarker = '>';
+    char rc;
+
+    while (Serial.available() > 0 && newData == false) {
+        rc = Serial.read();
+
+        if (recvInProgress == true) {
+            if (rc != endMarker) {
+                receivedChars[ndx] = rc;
+                ndx++;
+                if (ndx >= numChars) {
+                    ndx = numChars - 1;
+                }
+            }
+            else {
+                receivedChars[ndx] = '\0'; // caractere de terminação
+                recvInProgress = false;
+                ndx = 0;
+                newData = true;
+            }
+        }
+
+        else if (rc == startMarker) {
+            recvInProgress = true;
+        }
+    }
+}
+
+//=================================================================
+
+void parseData() {      // Dividir a mensagem em partes
+
+    char * strtokIndx; // Índice de srttok()
+
+    strtokIndx = strtok(tempChars,",");     // Pegar primeira parte (String)
+    strcpy(messageFromPC, strtokIndx);      // Armazenar em messageFromPC 
+ 
+    strtokIndx = strtok(NULL, ",");         // Continuar a dividir a mensagem
+    integerFromPC = atoi(strtokIndx);       // Converte para interiro e armazena na variável
+
+    strtokIndx = strtok(NULL, ",");         // Repete a operação anterior
+    integerFromPC2 = atoi(strtokIndx);      // Repetir para quantas partes forem necessárias
+
+    // strtokIndx = strtok(NULL, ",");
+    // floatFromPC = atof(strtokIndx);      // Caso seja necessário receber um float
+
 }
