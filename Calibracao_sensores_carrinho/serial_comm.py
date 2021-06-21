608c4fc2475c14f181dd6125236f28d2c7cf4c5b
Tux <ruster1506@gmail.com>
2021-06-21 09:40:59 -0400

    Script para pegar dados dos sensores

---
+++ Calibracao_sensores_carrinho/serial_comm.py
@@ -0,0 +1,19 @@
+import serial
+encoding = 'utf-8'
+
+
+ser = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=2)
+
+
+while True:
+
+    try:
+        x = ser.readline()
+        x = x.decode(encoding)
+
+        with open("serial_data.csv", "a") as f:
+            f.write(x)
+
+        print(x)
+    except KeyboardInterrupt:
+        ser.close()
