38f6364a6fdeb6a411455326c4cd1bca52f5f54e
João Aristeu <jasc.eai18@uea.edu.br>
2021-08-03 23:53:08 -0400

    Código comentado (e espaçado, Eduardo) para teste do carro com os valores de velocidade calculados.

--- teste_carro/src/teste_angulacao_carro.cpp
+++ teste_carro/src/teste_angulacao_carro.cpp
@@ -5,55 +5,77 @@
 //Lado direito
 AF_DCMotor motor1(1);
 AF_DCMotor motor2(2);
+
 //Lado esquerdo
 AF_DCMotor motor3(3);
 AF_DCMotor motor4(4);
+
 //Raio da roda
 #define raio_roda 0.0325
+
 // Pulsos por revolução do motor
 #define ENC_COUNT_REV 341.2
+
 //Lado direito
 // Pino de interrupção (Phase A). Rastreia a contagem dos pulsos.
 #define ENC_IN_RIGHT_A 19
+
 // Segundo pino digital (Phase B) do encoder. Rastreia o sentido de rotação.
 #define ENC_IN_RIGHT_B 47
+
 //Lado Esquerdo
 // Pino de interrupção (Phase A). Rastreia a contagem dos pulsos.
 #define ENC_IN_LEFT_A 18
+
 // Segundo pino digital (Phase B) do encoder. Rastreia o sentido de rotação.
 #define ENC_IN_LEFT_B 50
+
 // True = Frente; False = Trás;
 boolean direcao_direita = true;
 boolean direcao_esquerda = true;
+
 // Contador de pulsos da roda do lado direito
 volatile long pulsos_roda_direita = 0;
+
 // Contador de pulsos da roda do lado direito
 volatile long pulsos_roda_esquerda = 0;
+
 // Intervalo de 1 segundo
 int intervalo = 500;
+
 // Variáveis para o Blynk Without delay
 long previousMillis = 0;
 long currentMillis = 0;
+
 // Intervalo de 1 segundo
 int intervalo2 = 1000;
+
 // Variáveis para o Blynk Without delay
 long previousMillis2 = 0;
 long currentMillis2 = 0;
+
 //Variáveis para o cálculo da velocidade
 // Variáveis para medir o RPM da roda direita
 float rpm_direito = 0;
+
 // Variáveis para medir o RPM da roda direita
 float rpm_esquerdo = 0;
+
 // Variável para medir a velocidade angular da roda direita
 float w_direito = 0;
+
 // Variável para medir a velocidade angular da roda esquerda
 float w_esquerdo = 0;
+
 // Variáveis para medir a velocidade linear
 float velocidade_linear_direita = 0;
 float velocidade_linear_esquerda = 0;
+
 // Constante para converter rpm em radiano
 const float rpm_to_radiano = 0.10471975512;
+
 //=========================================
+
 const byte numChars = 64;
 char receivedChars[numChars];
 char tempChars[numChars];        // temporary array for use when parsing
@@ -62,11 +84,15 @@ char messageFromPC[numChars] = {0};
 int mensagemVelocidade = 0;
 int mensagemInverte = 0;
 boolean newData = false;
+
 //=========================================
+
 int velocidadeD = 0;
 int velocidadeE = 0;
 bool sentido = false;
+
 //=========================================
+
 void parseData() {      // split the data into its parts
   char copy_receivedChars[sizeof(receivedChars)] = "";
   strcpy(copy_receivedChars, receivedChars);
@@ -78,7 +104,9 @@ void parseData() {      // split the data into its parts
   // strtokIndx = strtok(NULL, ",");
   // floatFromPC = atof(strtokIndx);     // convert this part to a float
 }
+
 //=========================================
+
 void recvWithStartEndMarkers() {
   static boolean recvInProgress = false;
   static byte ndx = 0;
@@ -107,7 +135,9 @@ void recvWithStartEndMarkers() {
     }
   }
 }
+
 //=========================================
+
 void aguarda_mensagem(){
 recvWithStartEndMarkers();
   if (newData == true) {
@@ -115,6 +145,7 @@ recvWithStartEndMarkers();
     parseData();
     newData = false;
   }
+
   //Valores vindo dos sliders
   //Esperando a string 'value0' para a velocidade da roda esquerda
   if (strcmp(messageFromPC, "value0") == 0) {
@@ -134,8 +165,11 @@ recvWithStartEndMarkers();
     sentido = !sentido;
   }
 }
+
 //=========================================
+
 void roda_direita_contador() {
+
   // Lê o valor do encoder da roda direita
   int val = digitalRead(ENC_IN_RIGHT_B);
 
@@ -153,7 +187,9 @@ void roda_direita_contador() {
     pulsos_roda_direita--;
   }
 }
+
 //=========================================
+
 void roda_esquerda_contador() {
   // Lê o valor do encoder da roda direita
   int val = digitalRead(ENC_IN_LEFT_B);
@@ -172,12 +208,15 @@ void roda_esquerda_contador() {
     pulsos_roda_esquerda--;
   }
 }
+
 //=========================================
+
 void calculo() {
   // Cálcula da velocidade do lado direito
   rpm_direito = (float)(pulsos_roda_direita * 60 / ENC_COUNT_REV);
   w_direito = rpm_direito * rpm_to_radiano;
   velocidade_linear_direita = raio_roda*w_direito; 
+
   // Cálculo da velocidade do lado esquerdo
   rpm_esquerdo = (float)(pulsos_roda_esquerda * 60 / ENC_COUNT_REV);
   w_esquerdo = rpm_esquerdo * rpm_to_radiano;
@@ -202,6 +241,7 @@ void calculo() {
   pulsos_roda_esquerda = 0;
 }
 //=========================================
+
 void controle() {
   //Frente
   if (sentido == true) {
@@ -234,27 +274,35 @@ void controle() {
   motor4.setSpeed(velocidadeE);
 }
 //=========================================
+
 void setup() {
   // Define os pinos do encoder
   pinMode(ENC_IN_RIGHT_A , INPUT_PULLUP);
   pinMode(ENC_IN_RIGHT_B , INPUT);
   pinMode(ENC_IN_LEFT_A, INPUT_PULLUP);
   pinMode(ENC_IN_LEFT_B, INPUT);
+
   // Abre a porta serial 1 com BAUDRATE=9600
   Serial.begin(9600);
+
   // Abre a porta serial 2 com BAUDRATE=9600
   Serial2.begin(9600);
+
   // Funções de interrupção externa
-attachInterrupt(digitalPinToInterrupt(ENC_IN_RIGHT_A), roda_direita_contador, RISING);
-attachInterrupt(digitalPinToInterrupt(ENC_IN_LEFT_A), roda_esquerda_contador, RISING);
+  attachInterrupt(digitalPinToInterrupt(ENC_IN_RIGHT_A), roda_direita_contador, RISING);
+  attachInterrupt(digitalPinToInterrupt(ENC_IN_LEFT_A), roda_esquerda_contador, RISING);
 }
 //=========================================
+
 void loop() {
    controle();
+
   // Resgitra o tempo de envio de informações
   currentMillis = millis();
+
   // Resgitra o tempo de cálculo da velocidade
   currentMillis2 = millis();
+
   // Depois de um segundo, mostra-se o número de pulsos.
   if (currentMillis - previousMillis > intervalo) {
     previousMillis = currentMillis;
@@ -265,6 +313,7 @@ void loop() {
     previousMillis2 = currentMillis2;
     calculo();
   }
+
   //função que recebe e atribui as mensagens às variáveis de velocidade
   aguarda_mensagem();
 }
