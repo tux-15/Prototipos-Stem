a993467de0f5a24260f039f0c491e96cc8e21d96
Tux <ruster1506@gmail.com>
2021-09-13 13:27:23 -0400

    testando roteamento

--- Servidor/app.js
+++ Servidor/app.js
@@ -17,15 +17,6 @@ var heartbeat = Esp.heartbeat;
 
 var Page = require("./classes/Page.js");
 
-// class Chatters {
-//   constructor(esp, page){
-//     this.esp = esp;
-//     this.page = page;
-//     console.log(this.esp, "is binding to ", this.page);
-//   };
-
-// }
-
 const wss = new WebSocket.Server({
   port: 1801,
 });
@@ -45,7 +36,7 @@ let rooms = [];
  * JSON map:
  * {
  *    meta: indica contexto(manipulador/carrinho)
- *    start: indica se um esp/pagina entrou no servido
+ *    start: indica se um esp/pagina entrou no servidor
  *    to: id do alvo
  *    data: [{angulo0: 110}, {angulo1: 234}] etc etc //opcional
  * } 
@@ -61,53 +52,44 @@ wss.on('connection', function connection(ws, request) {
     messageJson = JSON.parse(message);
 
     if(messageJson['start'] == "ESP_on"){
-
-      global.esps.push(new Esp(ws, request.socket.remoteAddress, true));
-
+      var ip = request.socket.remoteAddress
+      global.esps.push(new Esp(ws, ip, true));
       console.log("new ESP: ", global.esps[global.esps.length-1].id);
     };
 
     if(messageJson['start'] == "page_on"){
-      var ip = request.socket.remoteAddress;
-      global.pages.push(new Page(ws, ip, messageJson['to'], messageJson['meta']));
-      lastPage = global.pages[global.pages.length-1];
 
+      var ip = request.socket.remoteAddress;
+
+      global.pages.push(new Page(ws, ip, messageJson['to'], messageJson['meta']));
+
+      lastPage = global.pages[global.pages.length-1];
       rooms.push({"pageId": lastPage.id, "espId": lastPage.pageEsp});
       console.log(rooms);
-
-      // console.log("pageEsp: ", global.pages[global.pages.length-1].pageEsp);
-      // console.log("meta: ", global.pages[global.pages.length-1].meta);
-      // console.log("new page: ", global.pages[global.pages.length-1].id);
-
-      //rooms.push(new Chatters(indexRouter.participants[0], indexRouter.participants[1]));
     };
 
     var wsId = ws['_socket']['_peername']['address'].slice(7);
 
-    //console.log("from: ", wsId);
-
-
     //TESTAR AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
 
-  // if(rooms[0] != undefined){
-  //   rooms.forEach(room => {
-  //     if (wsId == room.pageId){
-  //       //sconsole.log("a página foi achada");
-  //       global.esps.forEach(esp =>{
-  //         if (room.espId = esp.id){
-  //           //console.log("achou esp");
-  //           esp.connection.send(message);
-  //         };
-  //       });
-  //     };
-  //   });
-  // };
+  if(rooms[0] != undefined){
+    rooms.forEach(room => {
+      if (wsId == room.pageId){
+        //sconsole.log("a página foi achada");
+        global.esps.forEach(esp =>{
+          if (room.espId = esp.id){
+            //console.log("achou esp");
+            console.log("Page", room.pageId, "is sending messages to ESP", room.espId);
+            esp.connection.send(message);
+          };
+        });
+      };
+    });
+  };
 
-  esps.forEach(esp => esp.connection.send(message));
-  console.log('received: %s', message);
+  //esps.forEach(esp => esp.connection.send(message));
+  //console.log('received: %s', message);
   });
-
-  //ws.send('Connected to Server\n');
 });
 
 var app = express();
--- Servidor/classes/ESP.js
+++ Servidor/classes/ESP.js
@@ -1,19 +1,9 @@
-function noop() {};  
-
-function removeItemOnce(arr, value) {
-  var index = arr.indexOf(value);
-  if (index > -1) {
-    arr.splice(index, 1);
-  };
-  return;
-};
-
 class Esp {
     constructor(connection, ip, status){
       this.connection = connection;
-      this.ip = ip;
-      this.status = status
       this.id = ip.toString().slice(17);
+      this.status = status
+
     };
 
     static heartbeat(wsObject) {
@@ -39,8 +29,18 @@ class Esp {
         esp.status = false;
         esp.connection.ping(noop);
       });
-    }
+    };
 
-  }
+  };
+
+function noop() {};  
+
+function removeItemOnce(arr, value) {
+  var index = arr.indexOf(value);
+  if (index > -1) {
+    arr.splice(index, 1);
+  };
+  return;
+};
 
 module.exports = Esp;
