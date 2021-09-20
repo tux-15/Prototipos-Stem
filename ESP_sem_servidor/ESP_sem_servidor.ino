60b03354dbc9a1082916e25184e4b31af0f0df83
Tux <ruster1506@gmail.com>
2021-09-20 15:13:09 -0400

    manipulador com JSON / ESP em andamento

--- ESP_sem_servidor/ESP_sem_servidor.ino
+++ ESP_sem_servidor/ESP_sem_servidor.ino
@@ -1,7 +1,6 @@
 #include <ESP8266WiFi.h>        // WiFi
 #include <ESP8266WiFiMulti.h>   // Armazenar credenciais de múltiplas redes e conectar-se na mais forte
 #include <WebSocketsClient.h>   // WebSocket
-#include <ArduinoJson.h>
 
 ESP8266WiFiMulti wifiMulti;     // Obejto para gerenciar credenciais de rede
 WebSocketsClient webSocket; // WebSocket
@@ -10,26 +9,28 @@ const int port = 1801;
 const char * ip = "192.168.2.105";
 
 long previousMillis = 0; 
-long interval = 100;  // (em milissegundos) -> define o tempo de "delay"
+long interval = 500;  // (em milissegundos) -> define o tempo de "delay"
 
 void startWiFi();
 void startWebSocket();
 void sendMessageWs();
-void getJson();
 
 void setup() {
 
+  delay(100);
   Serial.begin(9600);
-  delay(500);
+  delay(250);
   
-  Serial.println("\nESP ready");
+  Serial.println("\n\rESP ready");
   
-  startWiFi();
-  startWebSocket();
+  //startWiFi();
+  //startWebSocket();
 
 }
 
 void loop() {
+
+  //getJson();
 
   unsigned long currentMillis = millis(); //Usado pelo blink without delay
 
@@ -37,14 +38,11 @@ void loop() {
   if(currentMillis - previousMillis > interval) { //Se o tempo especificado tiver passado, executar bloco de código
 
     long intervalo = currentMillis - previousMillis;
-
     previousMillis = currentMillis;
-    
     //sendMessageWs();
   }
 
-  //getJson();
-  webSocket.loop();
+  //webSocket.loop();
 }
 
 void startWiFi(){
@@ -54,6 +52,8 @@ void startWiFi(){
   wifiMulti.addAP("carreta_stem01", "Cs2k21!!");
   wifiMulti.addAP("MUSTAFA_2.4G", "96143008");
   wifiMulti.addAP("Seixas_Net", "Mayum647");
+  wifiMulti.addAP("Tux", "1234abcd");
+  
 
 
   Serial.println("Connecting");
@@ -66,16 +66,11 @@ void startWiFi(){
   Serial.println(WiFi.SSID());             // Nome da rede
   Serial.print("IP address:\t");
   Serial.print(WiFi.localIP());            // Ip do esp na rede local
-  Serial.print("");
-
-
   Serial.println("\r\n");
-
 }
 
 void startWebSocket() { // Inicializa o webSocket
   webSocket.begin(ip, port, "/");
-  
   webSocket.onEvent(webSocketEvent);          // função de callback para eventos que acontecerem no webSocket
   
   Serial.println("WebSocket client started.");
@@ -112,37 +107,11 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
       
     case WStype_PONG:
       // answer to a ping we send
-      Serial.printf("\r[WSc] get pong\n");
+      //Serial.printf("\r[WSc] get pong\n");
       break;       
   }
 }
-void sendMessageWs(const char * payload){
-    webSocket.sendTXT(payload);
-}
 
-void getJson(){
-  
-  if(Serial.available()){
-    StaticJsonDocument<300> doc;
-    DeserializationError err = deserializeJson(doc, Serial);
-    
-    if (err == DeserializationError::Ok) 
-    {
-      // Print the values
-      // (we must use as<T>() to resolve the ambiguity)
-//      Serial.print("timestamp = ");
-//      Serial.println(doc["timestamp"].as<long>());
-//      Serial.print("value = ");
-//      Serial.println(doc["value"].as<int>());
-    } 
-    else {
-      // Print error to the "debug" serial port
-      Serial.print("deserializeJson() returned ");
-      Serial.println(err.c_str());
-  
-      // Flush all bytes in the "link" serial port buffer
-      while (Serial.available() > 0)
-        Serial.read();
-    }
-  }
+void sendMessageWs(String& payload){//const char * payload){
+    webSocket.sendTXT(payload);
 }
--- manipulador/manipulador.ino
+++ manipulador/manipulador.ino
@@ -1,19 +1,5 @@
 #include <Servo.h>
-
-const byte numChars = 64;       //Número máximo de caracteres por mensagem
-char receivedChars[numChars];   //Armazenar mensagem
-char tempChars[numChars];       // Array temporário para parsing
-
-// Armazenar mensagem depois do parsing
-
-char messageFromPC[numChars] = {0};
-int integerFromPC = 0;
-int integerFromPC2 = 0;
-
-//Adicionar mais conforme necessidade
-//float floatFromPC = 0.0;
-
-boolean newData = false;
+#include <ArduinoJson.h>
 
 long previousMillis = 0; 
 long interval = 1000;  
@@ -24,10 +10,12 @@ Servo servo3;
 Servo servo4;
 Servo servo5;
 
-void checkNewData();
 void sendMessage();
 void moveServo();
 void startPosition();
+void getJson();
+
+const char * servo[20];
 
 void setup() {
   
@@ -43,29 +31,25 @@ void setup() {
 
   startPosition();
 
-  Serial.println("<Manipulador iniciando, 0, 0>");
-
 }
 
 void loop() {
-
-  sendMessage("Manipulador", 1, 1);
-  recvWithStartEndMarkers();
-  checkNewData();
-  moveServo(messageFromPC, integerFromPC);
+  
+  getJson();
+  //moveServo(messageFromPC, integerFromPC);
 
 }
 
 void startPosition(){
-  servo1.write(77);
-  servo2.write(0);
-  servo3.write(10);
-  servo4.write(141);
-  servo5.write(90);
+  servo1.write(93);
+  servo2.write(80);
+  servo3.write(15);
+  servo4.write(94);
+  servo5.write(60);
   
 }
 
-void moveServo(char servo[numChars], int angle){
+void moveServo(const char* servo, int angle){
   
   if (strcmp(servo, "value0") == 0){
     servo1.write(angle);
@@ -89,79 +73,40 @@ void moveServo(char servo[numChars], int angle){
 }
 
 
-void sendMessage(char message[numChars], int num1, int num2){
+//void sendMessage(char message[numChars], int num1, int num2){
+//
+//  unsigned long currentMillis = millis();
+//  
+//  if(currentMillis - previousMillis > interval) {
+//    previousMillis = currentMillis;
+//    Serial.print("<");
+//    Serial.print(message);Serial.print(", ");
+//    Serial.print(num1);Serial.print(", ");
+//    Serial.print(num2);Serial.println(">");
+//  }
+//}
 
-  unsigned long currentMillis = millis();
+void getJson(){
   
-  if(currentMillis - previousMillis > interval) {
-    previousMillis = currentMillis;
-    Serial.print("<");
-    Serial.print(message);Serial.print(", ");
-    Serial.print(num1);Serial.print(", ");
-    Serial.print(num2);Serial.println(">");
-  }
-}
-
-void checkNewData(){
-  
-  if (newData == true) {
-    strcpy(tempChars, receivedChars);
-    parseData();
-    newData = false;
-  }
-}
-
-void recvWithStartEndMarkers() {
-    static boolean recvInProgress = false;
-    static byte ndx = 0;
-    char startMarker = '<';
-    char endMarker = '>';
-    char rc;
-
-    while (Serial.available() > 0 && newData == false) {
-        rc = Serial.read();
-
-        if (recvInProgress == true) {
-            if (rc != endMarker) {
-                receivedChars[ndx] = rc;
-                ndx++;
-                if (ndx >= numChars) {
-                    ndx = numChars - 1;
-                }
-            }
-            else {
-                receivedChars[ndx] = '\0'; // caractere de terminação
-                recvInProgress = false;
-                ndx = 0;
-                newData = true;
-            }
-        }
-
-        else if (rc == startMarker) {
-            recvInProgress = true;
-        }
+  if(Serial.available()>2){
+    StaticJsonDocument<100> doc;
+    DeserializationError err = deserializeJson(doc, Serial);
+//    ReadLoggingStream loggingStream(Serial3, Serial);
+//    DeserializationError err = deserializeJson(doc, loggingStream);
+    
+    if (err == DeserializationError::Ok) 
+    {
+      const char* servo = doc["slider"].as<const char*>();
+      int angle = doc["angle"].as<int>();
+      Serial.println(servo);
+      Serial.println(angle);
+      moveServo(servo, angle);
+    } 
+    else {
+//      Serial.print("\ndeserializeJson() returned ");
+//      Serial.println(err.c_str());
+      while (Serial.available() > 0)
+        Serial.read();
     }
-}
-
-//============
-
-void parseData() {      // Dividir a mensagem em partes
-
-    char copy_receivedChars[sizeof(receivedChars)] = "";
-    strcpy(copy_receivedChars, receivedChars);
-
-    char * strtokIndx; // Índice de srttok()
-
-    strtokIndx = strtok(copy_receivedChars,",");     // Pegar primeira parte (String)
-    strcpy(messageFromPC, strtokIndx);      // Armazenar em messageFromPC 
- 
-    strtokIndx = strtok(NULL, ",");         // Continuar a dividir a mensagem
-    integerFromPC = atoi(strtokIndx);       // Converte para interiro e armazena na variável
-
-    strtokIndx = strtok(NULL, ",");         // Repete a operação anterior
-    integerFromPC2 = atoi(strtokIndx);      // Repetir para quantas partes forem necessárias
-
-    // strtokIndx = strtok(NULL, ",");
-    // floatFromPC = atof(strtokIndx);      // Caso seja necessário receber um float
-
+  }
 }
