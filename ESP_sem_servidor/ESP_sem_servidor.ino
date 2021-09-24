0866f98c4b58f7daf0808e0112ec54dae2d5c198
Tux <ruster1506@gmail.com>
2021-09-24 10:47:09 -0400

    esp com json bidirecional

--- ESP_sem_servidor/ESP_sem_servidor.ino
+++ ESP_sem_servidor/ESP_sem_servidor.ino
@@ -5,44 +5,39 @@
 ESP8266WiFiMulti wifiMulti;     // Obejto para gerenciar credenciais de rede
 WebSocketsClient webSocket; // WebSocket
 
+const byte numChars = 32;
+char receivedChars[numChars];
+
+boolean newData = false;
+
 const int port = 1801;
 const char * ip = "192.168.2.105";
 
 long previousMillis = 0; 
-long interval = 500;  // (em milissegundos) -> define o tempo de "delay"
+long interval = 100;  // (em milissegundos) -> define o tempo de "delay"
 
 void startWiFi();
 void startWebSocket();
 void sendMessageWs();
+void sendMessage();
 
 void setup() {
 
-  delay(100);
+  delay(500);
   Serial.begin(9600);
-  delay(250);
   
   Serial.println("\n\rESP ready");
   
-  //startWiFi();
-  //startWebSocket();
+  startWiFi();
+  startWebSocket();
 
 }
 
 void loop() {
-
-  //getJson();
-
-  unsigned long currentMillis = millis(); //Usado pelo blink without delay
-
-  //Colocar envio de mensagens aqui
-  if(currentMillis - previousMillis > interval) { //Se o tempo especificado tiver passado, executar bloco de código
-
-    long intervalo = currentMillis - previousMillis;
-    previousMillis = currentMillis;
-    //sendMessageWs();
-  }
-
-  //webSocket.loop();
+  
+  recvWithStartEndMarkers();
+  sendMessage();
+  webSocket.loop();
 }
 
 void startWiFi(){
@@ -53,8 +48,6 @@ void startWiFi(){
   wifiMulti.addAP("MUSTAFA_2.4G", "96143008");
   wifiMulti.addAP("Seixas_Net", "Mayum647");
   wifiMulti.addAP("Tux", "1234abcd");
-  
-
 
   Serial.println("Connecting");
   while (wifiMulti.run() != WL_CONNECTED) {  // Esperar WiFi conectar
@@ -74,6 +67,25 @@ void startWebSocket() { // Inicializa o webSocket
   webSocket.onEvent(webSocketEvent);          // função de callback para eventos que acontecerem no webSocket
   
   Serial.println("WebSocket client started.");
+}
+
+void sendMessageWs(const char * payload){
+    webSocket.sendTXT(payload);
+}
+
+void sendMessage() {
+  unsigned long currentMillis = millis(); 
+  if(currentMillis - previousMillis > interval) {
+
+    if(newData == true){
+      sendMessageWs(receivedChars);
+      Serial.print("Received from Arduino: ");
+      Serial.println(receivedChars);
+      newData = false;
+    }
+    
+    previousMillis = currentMillis;
+  }
 }
 
 void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
@@ -112,6 +124,36 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
   }
 }
 
-void sendMessageWs(String& payload){//const char * payload){
-    webSocket.sendTXT(payload);
+void recvWithStartEndMarkers() {
+    static boolean recvInProgress = false;
+    static byte ndx = 0;
+    char startMarker = '{';
+    char endMarker = '}';
+    char rc;
+ 
+    while (Serial.available() > 0 && newData == false){
+        rc = Serial.read();
+        if (recvInProgress == true){
+            if (rc != endMarker){
+                receivedChars[ndx] = rc;
+                ndx++;
+                if (ndx >= numChars){
+                    ndx = numChars - 1;
+                }
+            }
+            else{
+                receivedChars[ndx] = endMarker;
+                ndx++;
+                receivedChars[ndx] = '\0'; // terminate the string
+                recvInProgress = false;
+                ndx = 0;
+                newData = true;
+            }
+        }
+        else if (rc == startMarker){
+            recvInProgress = true;
+            receivedChars[ndx] = rc;
+            ndx++;
+        }
+    }
 }
