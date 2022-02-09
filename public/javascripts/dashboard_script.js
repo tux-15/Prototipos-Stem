f0b79b4de55e762f61543826a02461f092c58161
Santiago Monteiro <santiago.a.monteiro@gmail.com>
2022-02-09 01:26:04 -0400

    rendering states on dashboard

--- public/javascripts/dashboard_script.js
+++ public/javascripts/dashboard_script.js
@@ -1,9 +1,19 @@
 async function findPrototypeData() {
-
   const response = await fetch("javascripts/json.json");
   const prototypeData = await response.json();
 
-  console.log(prototypeData)
+  const {
+    runningMachineState,
+    arm1State,
+    arm2State,
+    cart: { cartState, cartSpeed },
+  } = prototypeData;
+
+
+  renderStateOfRunningMachine(runningMachineState);
+  renderStateOfArm1(arm1State);
+  renderStateOfArm2(arm2State);
+  renderVelocityOfCar(cartState, cartSpeed);
 }
 
 function changePrototypeCardState(element, state) {
@@ -12,7 +22,7 @@ function changePrototypeCardState(element, state) {
     element.parentElement.classList.add("on");
   } else {
     element.innerText = "Off";
-    element.parentElement.classList.remove("on")
+    element.parentElement.classList.remove("on");
   }
 }
 
@@ -24,16 +34,22 @@ function renderStateOfRunningMachine(state) {
 function renderStateOfArm1(state) {
   const arm1 = document.querySelector("#arm1");
   changePrototypeCardState(arm1, state);
-};
+}
 
 function renderStateOfArm2(state) {
   const arm2 = document.querySelector("#arm2");
   changePrototypeCardState(arm2, state);
-};
+}
 
-function renderVelocityOfCar(speed) {
+function renderVelocityOfCar(state, speed) {
   const cartSpeed = document.querySelector("#cart-speed");
-  
-};
+  cartSpeed.innerText = speed;
+
+  if (state === "on") {
+    cartSpeed.parentElement.classList.add("on");
+  } else {
+    cartSpeed.parentElement.classList.remove("on");
+  }
+}
 
 findPrototypeData();
