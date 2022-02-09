09055b7ab0c4567bb2ca038dd93953fa034b06ae
Santiago Monteiro <santiago.a.monteiro@gmail.com>
2022-02-09 01:18:31 -0400

    creation of functions for rendering state of prototypes using a fake json

--- public/javascripts/dashboard_script.js
+++ public/javascripts/dashboard_script.js
@@ -1,11 +1,39 @@
-function switchState(dataString) {
-  const runningMachine = document.querySelector("#running-machine");
-  const arm1 = document.querySelector("#arm1");
-  const arm2 = document.querySelector("#arm2");
-  const cartSpeed = document.querySelector("#cart-speed");
+async function findPrototypeData() {
 
-  runningMachine.innerText = "On"
-  runningMachine.parentElement.classList.add("on");
+  const response = await fetch("javascripts/json.json");
+  const prototypeData = await response.json();
+
+  console.log(prototypeData)
 }
 
-switchState();
+function changePrototypeCardState(element, state) {
+  if (state === "on") {
+    element.innerText = "On";
+    element.parentElement.classList.add("on");
+  } else {
+    element.innerText = "Off";
+    element.parentElement.classList.remove("on")
+  }
+}
+
+function renderStateOfRunningMachine(state) {
+  const runningMachine = document.querySelector("#running-machine");
+  changePrototypeCardState(runningMachine, state);
+}
+
+function renderStateOfArm1(state) {
+  const arm1 = document.querySelector("#arm1");
+  changePrototypeCardState(arm1, state);
+};
+
+function renderStateOfArm2(state) {
+  const arm2 = document.querySelector("#arm2");
+  changePrototypeCardState(arm2, state);
+};
+
+function renderVelocityOfCar(speed) {
+  const cartSpeed = document.querySelector("#cart-speed");
+  
+};
+
+findPrototypeData();
