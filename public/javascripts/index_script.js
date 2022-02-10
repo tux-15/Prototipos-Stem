281b591500b1406007d63fbe5f779fd2fdb011fa
Santiago Monteiro <santiago.a.monteiro@gmail.com>
2022-02-10 11:29:27 -0400

    check type of esp to render homepage buttons

--- public/javascripts/index_script.js
+++ public/javascripts/index_script.js
@@ -1,13 +1,17 @@
 window.setInterval(searchESPs, 1000);
 
 function searchESPs() {
-  var xhttp = new XMLHttpRequest();
+  const xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-      // this.readyState == 4 && this.status == 200
       // esp_list = JSON.parse(this.responseText);
-      esp_list = [{ id: "001" }, { id: "002" }, { id: "003" }]; // this line
-      let mainDiv = document.getElementById("esps_ativos");
+      esp_list = [
+        { id: "001", type: "manual" },
+        { id: "002", type: "manual" },
+        { id: "003", type: "sistema" },
+      ]; // this line
+
+      const mainDiv = document.getElementById("esps_ativos");
       let htmlContainer = ``;
 
       if (mainDiv.hasChildNodes()) {
@@ -17,48 +21,54 @@ function searchESPs() {
         // mainDiv.innerHTML = ``;
       }
 
-      if (esp_list[0] != undefined) {
-        esp_list.forEach((esp) => {
-          if (esp.taken != true) {
-            htmlContainer += `<!-- <div class="description-esp">${esp.id}</div> -->
-  <div class="container-monitoring-button">
-    <a class="monitoring-button"
-    onclick="
-      startRobot(${esp.id}, 'dashboard');
-    "
-    >
-    <span>Monitoramento</br>ID: ${esp.id}</span>
-    </a>
-  </div>
+      if (esp_list.length > 0) {
+        const systemEsp = esp_list.find((esp) => esp.type === "sistema");
 
-  <div class="container-img">
-    <img src="./images/index/robot-arm.png" class="robot-img">
-    <img src="./images/carrinho/Chassi_II.png" class="car-img">
-  </div>
-  
-  <div class="container-arm-car-button">
-    <a
-      class="arm-button"
-      onclick="
-        startRobot(${esp.id}, 'manipulador');
-      "
-    >
-    <img src="./images/index/robotic-arm.svg" />
-      <span>Braço robótico ID: ${esp.id}</span>
-    </a>
-    <a
-      class="car-button"
-      onclick="
-        startRobot(${esp.id}, 'carrinho');
-      "
-    >
-      <img src="./images/index/car.svg" />
-      <span>Carrinho </br>ID: ${esp.id}</span>
-    </a>
-  </div>
-  <div class="divisor-esps"></div>`;
+        if (systemEsp) {
+          htmlContainer += 
+          `<div class="container-monitoring-button">
+            <a class="monitoring-button"
+              onclick="
+              startRobot(${systemEsp.id}, 'dashboard');
+              "
+            >
+              <span>Monitoramento</br>ID: ${systemEsp.id}</span>
+            </a>
+          </div>`;
+        }
+
+        esp_list.forEach((esp) => {
+          if (!esp.taken) {
+            htmlContainer += `
+              <div class="container-img">
+                <img src="./images/index/robot-arm.png" class="robot-img">
+                <img src="./images/carrinho/Chassi_II.png" class="car-img">
+              </div>
+              
+              <div class="container-arm-car-button">
+                <a
+                  class="arm-button"
+                  onclick="
+                    startRobot(${esp.id}, 'manipulador');
+                  "
+                >
+                <img src="./images/index/robotic-arm.svg" />
+                  <span>Braço robótico ID: ${esp.id}</span>
+                </a>
+                <a
+                  class="car-button"
+                  onclick="
+                    startRobot(${esp.id}, 'carrinho');
+                  "
+                >
+                  <img src="./images/index/car.svg" />
+                  <span>Carrinho </br>ID: ${esp.id}</span>
+                </a>
+              </div>
+              <div class="divisor-esps"></div>`;
           }
         });
+
         mainDiv.innerHTML = htmlContainer;
       }
     }
